6b43445c77ac4b0e85ebdbb1b391f693b9349f2c
CIA0CIA0 <cia0cia08611@gmail.com>
2023-06-16 13:05:04 +0800

    实现前端登录注册忘记密码【vue3】

---
+++ src/components/welcome/LoginPage.vue
@@ -0,0 +1,94 @@
+<template>
+    <div style="text-align: center;margin: 0 20px">
+        <div style="margin-top: 150px" >
+            <div style="font-size: 36px;font-weight: bold">
+                <img src="@/assets/logo.gif" alt="logo" style="height: 50px;margin-right: 8px;"/>ニコニコ
+            </div>
+            <div style="font-size: 14px;color: grey">为美好的世界献上祝福(゜-゜)つロ干杯~</div>
+        </div>
+        <div style="margin-top: 50px">
+            <el-input v-model="form.username" type="text" placeholder="用户名/邮箱">
+                <template #prefix>
+                    <el-icon><User /></el-icon>
+                </template>
+            </el-input>
+            <el-input v-model="form.password" type="password" style="margin-top: 10px" placeholder="密码" @blur="isFocused = false" @focus="isFocused = true">
+                <template #prefix>
+                    <el-icon><Lock /></el-icon>
+                </template>
+            </el-input>
+        </div>
+        <el-row style="margin-top: 5px">
+            <el-col :span="12" style="text-align: left">
+                <el-checkbox v-model="form.remember" label="记住我"/>
+            </el-col>
+            <el-col :span="12" style="text-align: right">
+                <el-link @click="router.push('/forget')">忘记密码？</el-link>
+            </el-col>
+        </el-row>
+        <div style="margin-top: 40px">
+            <el-button @click="login()" style="width: 270px" type="success" plain>立即登录</el-button>
+        </div>
+        <el-divider>
+            <span style="color: grey;font-size: 13px">没有账号</span>
+        </el-divider>
+        <div>
+            <el-button style="width: 270px" @click="router.push('/register')" type="warning" plain>注册账号</el-button>
+        </div>
+        <div class="welcome-icon">
+            <el-image :src="iconSrc"></el-image>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {User, Lock} from '@element-plus/icons-vue'
+import {reactive} from "vue";
+import {ElMessage} from "element-plus";
+import {get, post} from "@/net";
+import router from "@/router";
+import {useStore} from "@/stores";
+import { ref, computed } from 'vue';
+
+const store = useStore()
+
+const form = reactive({
+    username: '',
+    password: '',
+    remember: false
+})
+const isFocused = ref(false);
+const login = () => {
+    if(!form.username || !form.password) {
+        ElMessage.warning('请填写用户名和密码！')
+    } else {
+        post('/api/auth/login', {
+            username: form.username,
+            password: form.password,
+            remember: form.remember
+        }, (message) => {
+            ElMessage.success(message)
+            get('/api/user/currentUser', (message) => {
+                store.auth.user = message
+                router.push('/index')
+            }, () => {
+                store.auth.user = null
+            })
+        })
+    }
+}
+const iconSrc = computed(() => {
+    return isFocused.value
+        ? 'https://s2.loli.net/2023/06/15/jZiVS1qCgz8RGob.png'
+        : 'https://s2.loli.net/2023/06/12/JGLZ2tEjUn1zfK6.png';
+});
+</script>
+
+<style scoped>
+.welcome-icon {
+    position: absolute;
+    width: 7rem;
+    bottom: 0;
+    right: 0;
+}
+</style>
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+        <div style="flex: 1">
+            <el-image style="width: 100%;height: 100%" fit="cover" src="https://s2.loli.net/2023/04/21/O4WDpJLyPAHisgm.jpg"/>
+        </div>
+        <div class="welcome-title">
+            <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
+            <div style="margin-top: 20px">在这里你可以学习如何使用Java，如何搭建网站，并且与Java之父密切交流。</div>
+            <div style="margin-top: 5px">在这里你可以同性交友，因为都是男的，没有学Java的女生。</div>
+        </div>
+        <div style="width: 420px;background-color: white;z-index: 1">
+            <router-view v-slot="{ Component }">
+                <!--      transition为跳转动效          -->
+                <transition name="el-fade-in-linear" mode="out-in">
+                    <component :is="Component" style="height: 100%"/>
+                </transition>
+            </router-view>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.welcome-title {
+    position: absolute;
+    bottom: 30px;
+    left: 10px;
+    color: black;
+    text-shadow: 0 0 10px #b7b7b7;
+}
+
+</style>
